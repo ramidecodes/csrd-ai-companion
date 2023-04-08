39b5df512d6fcc90777cd5c5b051cbf5cf8e8d79
Ramiro <ramiro@kryha.io>
2023-04-08 17:59:10 +0200

    First commi

---
+++ embeddings/parser.ts
@@ -0,0 +1,22 @@
+// Load from pdf
+
+// Chunk into
+/**  Chunck into:
+ * * 1. Pages
+ * * 2. Paragraphs
+ * * 3. Sentences
+ * * 4. Words
+ * * 5. Characters
+ * * 6. Tokens
+ * * 7. Embeddings
+ */
+'
+
+// const getPages = async (pdf: PDFDocumentProxy) => {
+//     const pages = []
+//     for (let i = 0; i < pdf.numPages; i++) {
+//         const page = await pdf.getPage(i + 1)
+//         pages.push(page)
+//     }
+//     return pages
+// }
